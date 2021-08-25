602e55e226fd29c872e745fb86f553dafaebfb4b
David Meunier <david_meunier_79@hotmail.fr>
2021-08-25 17:25:13 +0200

    stderr_data

--- metrics.py
+++ metrics.py
@@ -17,6 +17,8 @@ def intraclass_correlation (path_data1,path_data2) :
     import subprocess
     ICC_cmd = ["MeasureImageSimilarity", "-d", "3",  "-m",  "CC[{},{},1,1]".format(path_data1, path_data2)]
     stdout_data, stderr_data = subprocess.Popen(ICC_cmd).communicate()
+    print(stdout_data)
+    print(stderr_data)
 
     val1 = float(stdout_data)
 
